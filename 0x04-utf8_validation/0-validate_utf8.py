fff0bb1cedc7873b9d13f8b20c7b5efe02118302
madiocre <wolvirex@gmail.com>
2024-07-23 22:20:25 +0300

    UTF8 added_multibyte_implement_fix-2

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -2,20 +2,6 @@
 """ utf8 validation """
 
 
-def num_of_bytes(num):
-    """ Checks for num of bytes"""
-    if num & 0xF0 == 0xF0:
-        return 3
-    elif num & 0xE0 == 0xE0:
-        return 2
-    elif num & 0xC0 == 0xC0:
-        return 1
-    elif num & 0x80 == 0x00:
-        return 0
-    else:
-        return -1
-
-
 def validUTF8(data):
     """
     Determines if a given data set represents a valid UTF-8 encoding.
@@ -38,12 +24,19 @@ def validUTF8(data):
     num_bytes = 0
     for num in data:
         if num_bytes == 0:
-            num_bytes = num_of_bytes(num)
-            if num_bytes == -1:
+            if num & 0xF0 == 0xF0:
+                num_bytes = 3
+            elif num & 0xE0 == 0xE0:
+                num_bytes = 2
+            elif num & 0xC0 == 0xC0:
+                num_bytes = 1
+            elif num & 0x80 == 0x00:
+                num_bytes = 0
+            else:
                 return False
 
         else:
-            if (num & 0xC2) != 0x80:
+            if num & 0xC2 != 0x80:
                 return False
 
     return num_bytes == 0

6cd1175110a4715c1e6eea00979d243a339dd99d
madiocre <wolvirex@gmail.com>
2024-08-13 07:36:37 +0300

    rotate-2d_final

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -4,5 +4,14 @@ Rotating an input matrix without returning it
 """
 
 def rotate_2d_matrix(matrix):
-    global matrix #to make sure to edit the variable
+    """ Function to rotate a 2d matrix counter clockwise     """
+    N = len(matrix)
+    
+    for x in range(0, int(N / 2)):
+        for y in range(x, N-x-1):
+            temp = matrix[x][y]
+            matrix[x][y] = matrix[y][N-1-x]
+            matrix[y][N-1-x] = matrix[N-1-x][N-1-y]
+            matrix[N-1-x][N-1-y] = matrix[N-1-y][x]
+            matrix[N-1-y][x] = temp
     

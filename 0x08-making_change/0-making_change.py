a1687cc95fa31b91ab7dff14464ecba4fb347e34
madiocre <wolvirex@gmail.com>
2024-08-22 20:54:50 +0300

    style fix

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -6,11 +6,11 @@ Least Change Coins
 
 def makeChange(coins, total):
     """ Function to Calculate coins """
-    coins.sort(reverse = True)
+    coins.sort(reverse=True)
     tot_coins = 0
     # print(coins)
     for coin in coins:
         while total >= coin:
             tot_coins += 1
             total -= coin
-    return tot_coins if total == 0 else -1
+    return tot_coins if total == 0 else - 1

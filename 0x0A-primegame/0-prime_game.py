a5e42e7c728828c206d5d604677b235afe226a7f
Madiocre <wolvirex@gmail.com>
2024-09-02 19:02:44 +0300

    PrimeGame Winner

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" Prime Game
+"""
+
+def count_primes_up_to(n):
+    """ Function to count prime numbers up to n using the Sieve of Eratosthenes. """
+    
+    if n < 2:
+        return 0
+    
+    prime = [True] * (n + 1)
+    prime[0] = prime[1] = False
+    
+    p = 2
+    while p * p <= n:
+        if prime[p]:
+            for i in range(p * p, n + 1, p):
+                prime[i] = False
+        p += 1
+
+    return sum(prime)
+
+def isWinner(x, nums):
+    """ Function to Display Rounds and return who won more rounds """
+    Ben = 0
+    Maria = 0
+
+    for num in nums:
+        if num == 1:
+            Ben += 1
+        else:
+            count = count_primes_up_to(num)
+            if count % 2 == 1:
+                Maria += 1
+            else:
+                Ben += 1
+
+    if Ben > Maria:
+        return 'Ben'
+    elif Maria > Ben:
+        return 'Maria'
+    else:
+        return None
